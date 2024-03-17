d68e4f04c5b3d641e050409d593bb0532db869ee
Davyd <davidoo1234e@gmail.com>
2024-03-17 09:55:14 +0100

    Initial Commit

---
+++ KahootClone/KahootCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  KahootCloneApp.swift
+//  KahootClone
+//
+//  Created by Davyd Darusenkov on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct KahootCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
